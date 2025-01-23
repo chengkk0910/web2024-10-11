2169373e34bd67cc501507a22f1d27c8746dadba
Kai <cgk0910@gmail.com>
2025-01-23 14:07:56 +0800

    add php public data

--- s20250123/php/data_public.php
+++ s20250123/php/data_public.php
@@ -12,6 +12,11 @@ $data = [
     's3' => 'cat'
 ];
 
+header('Access-Control-Allow-Origin: http://127.0.0.1:5501');
+header('Access-Control-Allow-Origin: http://kai.com');
+// header('Access-Control-Allow-Origin: *');
+
+
 // dd($data);
 
 echo json_encode($data);
