b5668d8f83c0e9aa4293e180094cc0e5717f25ce
Kai <cgk0910@gmail.com>
2025-01-23 14:10:23 +0800

    fix

--- s20250123/php/data_public.php
+++ s20250123/php/data_public.php
@@ -13,7 +13,6 @@ $data = [
 ];
 
 header('Access-Control-Allow-Origin: http://127.0.0.1:5501');
-header('Access-Control-Allow-Origin: http://kai.com');
 // header('Access-Control-Allow-Origin: *');
 
 
